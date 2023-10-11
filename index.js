ca08ed9dd4223285c4f8b2c5b0895c51669e068c
evY000 <000evan001@gmail.com>
2023-10-11 13:14:20 -0400

    dom

---
+++ index.js
@@ -0,0 +1,9 @@
+DOMSelectors = {
+            form : document.querySelector("#form"),
+            firstName : document.querySelector(".first-name"),
+            h2s: document.querySelectorALL("h2")
+};
+DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
+});
