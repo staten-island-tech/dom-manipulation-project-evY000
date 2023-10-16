03ffc6004231d2cf3202e072d106a4a6778fad07
evY000 <000evan001@gmail.com>
2023-10-16 13:15:23 -0400

    uikjhytgbn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-DOMSelectors = {
-            form : document.querySelector("#form"),
-            firstName : document.querySelector(".first-name"),
-            h2s: document.querySelectorALL("h2")
-};
+ const DOMSelectors = {
+            button:document.getElementById("btn"),
+            box:document.getElementById("container-box"),
+            input : document.querySelector("#input"), };
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
