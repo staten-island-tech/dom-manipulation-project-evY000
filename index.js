29df4113de303087d75b54511dab3e5f96ae162e
evY000 <000evan001@gmail.com>
2023-10-17 13:13:31 -0400

    rftgyhj

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
  const DOMSelectors = {
-            button:document.getElementById("btn"),
-            box:document.getElementById("container-box"),
-            input : document.querySelector("#input"), };
+            firstName: document.getElementById('first-name'),
+            form: document.getElementById("form"),
+            h2s: document.querySelectorAll("h2"),
+ };
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
 });
+const Cancel = {
+    
+}
