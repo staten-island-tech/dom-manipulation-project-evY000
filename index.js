38db6b6c498f991932817033929b6714e17ee303
evY000 <000evan001@gmail.com>
2023-10-24 13:14:57 -0400

    e ewrfgthy

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
             firstName: document.getElementById('first-name'),
             form: document.getElementById("form"),
             h2s: document.querySelectorAll("h2"),
+            inputclear : document.querySelectorAll('submit'),
+        
  };
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
+    DOMSelectors.inputclear.forEach(singlesubmit => singlesubmit.value = '');
 });
