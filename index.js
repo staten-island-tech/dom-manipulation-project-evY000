0f84f878d613af06a12ff49a132c0d37e2896495
evY000 <000evan001@gmail.com>
2023-10-26 13:12:36 -0400

    sazdfghjkl;'

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
             firstName: document.getElementById('first-name'),
             form: document.getElementById("form"),
             h2s: document.querySelectorAll("h2"),
-            inputclear : document.querySelectorAll('submit'),
-        
- };
+ }
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
-    DOMSelectors.inputclear.forEach(singlesubmit => singlesubmit.value = '');
-});
+    information()}
+);
+function information(){
+    const user ={
+        firstname: DOMSelectors.h2s.value
+    }
+    inject(user)
+    clear()
+}
+function clear(){
+    DOMSelectors.h2s.value = ''
+};
