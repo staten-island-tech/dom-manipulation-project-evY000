ed3e57fcc0ce908483f9ab4b91d9a14968b6d04b
evY000 <000evan001@gmail.com>
2023-11-08 09:03:09 -0500

    asdfghjkl;

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
  const DOMSelectors = {
             firstName: document.getElementById('first-name'),
             form: document.getElementById("form"),
-            h2s: document.querySelectorAll("h2"),
- }
+            h2s: document.querySelectorAll("h2"),}
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
@@ -10,8 +9,8 @@ DOMSelectors.form.addEventListener("submit", function (event) {
 );
 function information(){
     DOMSelectors.form.addEventListener("click",function(){
-    const user = DOMSelectors.firstName.value; 
-    DOMSelectors.form.insertAdjacentHTML("beforeend", "<h2> ${user} </h2>");
+    let user = DOMSelectors.firstName.value; 
+    DOMSelectors.form.insertAdjacentHTML("beforeend", '<h2> ${user} </h2>');
     clear(user)
     })};
 function clear(){
