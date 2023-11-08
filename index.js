779dcdc771674e0edbe0c8b670a37841612a9d36
evY000 <000evan001@gmail.com>
2023-11-08 10:51:48 -0500

    sWDEGRYTUIOP;LKIYJUYGFDS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
  const DOMSelectors = {
             firstName: document.getElementById('first-name'),
             form: document.getElementById("form"),
-            h2s: document.querySelectorAll("h2"),}
+            h2s: document.querySelectorAll("h2"),
+            deleteButton: document.createElement("button")};
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
@@ -10,9 +11,18 @@ DOMSelectors.form.addEventListener("submit", function (event) {
 function information(){
     DOMSelectors.form.addEventListener("click",function(){
     let user = DOMSelectors.firstName.value; 
-    DOMSelectors.form.insertAdjacentHTML("beforeend", '<h2> ${user} </h2>');
-    clear(user)
+    DOMSelectors.form.insertAdjacentHTML("afterend", `<h2 class="enter"> ${user} </h2> <button type ="button"> Delete</button>`);
+    clear(user);
+    deleteThing();
     })};
+    
 function clear(){
     DOMSelectors.firstName.value = ''
 };
+function deleteThing(){
+   
+    DOMSelectors.deleteButton.addEventListener("click", function(event){
+        event.target.user.remove()
+    });
+    
+}
