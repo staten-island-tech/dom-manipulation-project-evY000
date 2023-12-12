909a1c716e7b4476e5acf7fa48aaf8510aacc9cd
evY000 <000evan001@gmail.com>
2023-12-12 08:09:49 -0500

    asdfgh

--- index.js
+++ index.js
@@ -2,27 +2,29 @@
             firstName: document.getElementById('first-name'),
             form: document.getElementById("form"),
             h2s: document.querySelectorAll("h2"),
-            deleteButton: document.createElement("button")};
-DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value)
-    information()}
-);
+ }
+
+            
+
+    information()
+
 function information(){
-    DOMSelectors.form.addEventListener("click",function(){
+    DOMSelectors.form.addEventListener("submit",function(event){
+        event.preventDefault();
     let user = DOMSelectors.firstName.value; 
-    DOMSelectors.form.insertAdjacentHTML("afterend", `<h2 class="enter"> ${user} </h2> <button type ="button"> Delete</button>`);
-    clear(user);
-    deleteThing();
+    let input = DOMSelectors.form.insertAdjacentHTML("afterend", `<h2 class="enter"> ${user} </h2> <button type ="deletebutton"> Delete</button>`);
+     clear(user);
+    deleteThing(input);
     })};
     
 function clear(){
     DOMSelectors.firstName.value = ''
 };
 function deleteThing(){
-   
-    DOMSelectors.deleteButton.addEventListener("click", function(event){
-        event.target.user.remove()
-    });
+    DOMSelectors.deletebutton.addEventListener("click", function(event){
+        event.input.remove();
+    })
+    
+
     
 }
