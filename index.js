63fe2fc3e0b8a9b6ddeb107280cd28681104c784
evY000 <000evan001@gmail.com>
2024-01-01 23:20:22 -0500

    asdfghjk

--- index.js
+++ index.js
@@ -2,29 +2,26 @@
             firstName: document.getElementById('first-name'),
             form: document.getElementById("form"),
             h2s: document.querySelectorAll("h2"),
+            imageLink :document.getElementById("img"),
  }
-
-            
-
-    information()
-
+information()
 function information(){
     DOMSelectors.form.addEventListener("submit",function(event){
         event.preventDefault();
     let user = DOMSelectors.firstName.value; 
-    let input = DOMSelectors.form.insertAdjacentHTML("afterend", `<h2 class="enter"> ${user} </h2> <button type ="deletebutton"> Delete</button>`);
-     clear(user);
-    deleteThing(input);
-    })};
+    let image = DOMSelectors.imageLink.value;
+    DOMSelectors.form.insertAdjacentHTML("afterend", `<div id = "box"><h2 class="enter"> ${user}<br> <img src=${image} width="500" length="500"> </h2> <button id ="DeleteButton" class="delete" onclick="deleteThing()"> Delete </button></div>`);
+   clear(user);
+    })
+}
     
 function clear(){
     DOMSelectors.firstName.value = ''
+    DOMSelectors.imageLink.value = ''
 };
 function deleteThing(){
-    DOMSelectors.deletebutton.addEventListener("click", function(event){
-        event.input.remove();
-    })
-    
-
-    
+  box.remove() 
 }
+
+
+
